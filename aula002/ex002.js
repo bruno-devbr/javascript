6242bce59e8771236518b943d2c5b0cd7d50eada
Bruno Queiroz <147547055+bruno-devbr@users.noreply.github.com>
2024-11-11 21:15:05 -0300

    ex 02

---
+++ aula002/ex002.js
@@ -0,0 +1,10 @@
+const nome = 'Bruno';
+const idade = 16;
+const numeroGrande = 1000000000000000n;
+const isStudent = false;
+const unknown = undefined;
+const vazio = null;
+const id = Symbol("identificador");
+
+console.log(`${nome}, ${idade}, ${numeroGrande}, ${isStudent}, ${unknown}, ${vazio},`)
+console.log(id)
