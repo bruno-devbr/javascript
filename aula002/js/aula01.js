7126c678f2dd167b346a46fe0d8da5f64e1a0047
bruno-devbr <brunoqueiroz0707@gmail.com>
2024-11-17 19:14:30 -0300

    aula 02

---
+++ aula002/js/aula01.js
@@ -0,0 +1,40 @@
+/* Arquivo JS Externo */
+
+document.write("Olá, Mundo!");
+console.log("outro Olá, Mundo!");
+// alert("Olá, Mundo! em forma de alerta")
+
+let mensagem = "Minha mensagem";
+let mensagem2 = "Outra mensagem";
+let meuPeso = 83.5;
+let minhaAltura = 1.75;
+let minhaIdade = 40;
+let ehDoador = false;
+let teste = null;
+
+teste = "Algum conteudo"
+
+// CRIAR CONSTANTES e atribuir valor
+
+const PI = 3.1415;
+const TAXA = 0.5
+
+// ESCREVER CONTEUDO DE VARIAVEIS E CONSTANTES  
+
+document.write("<p> </p>Mensgame " + mensagem + "</p>");
+
+document.write(`Mensagem2: ${mensagem2}`);
+
+document.write("<p>Peso: " + meuPeso + "Kg </p>");
+
+document.write(`Altura: ${minhaAltura} m`);
+
+document.write("<p> Idade " + minhaIdade + " anos </p>");
+
+document.write(`Doador? ${ehDoador}`);
+
+document.write("<p>Teste: " + teste + "</p>");
+
+document.write("<p>PI " + PI + "</p>");
+
+document.write("<p>TAXA " + TAXA + "</p>");
