5690fa66bc6a9abecb7dbf996ca47325277b4b46
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-02-04 12:24:15 -0300

    ex004

---
+++ exercicios/ex004/ex04.js
@@ -0,0 +1,11 @@
+/* Arquivo JS */ 
+
+let Game = {
+    Jogo : 'Max Payne',
+    Desnvolvedora : 'Remedy Entertainament',
+    Lançamento : 2001
+};
+
+document.write(`Jogo: ${Game.Jogo} <br>`)
+document.write(`Desenvolvido por: ${Game.Desnvolvedora} <br>`)
+document.write(`Ano do Lançamento: ${Game.Lançamento} <br>`)
