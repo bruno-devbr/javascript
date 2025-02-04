c39a9341854ddd53a9f6558676f2a4adcea8deb6
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-02-04 12:07:11 -0300

    ex002

---
+++ exercicios/ex002/ex02.js
@@ -0,0 +1,12 @@
+/* Arquivo JS externo */
+
+const dia = 17;
+const mes = "03";
+const ano = 2006;
+
+const frase = "A persistência realiza o impossível.";
+const autor = "Provérbio Chinês"
+
+document.write(`${dia}/${mes}/${ano} <br>`);
+document.write(`${frase} <br>`);
+document.write(`${autor}`);
