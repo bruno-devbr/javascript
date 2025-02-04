eede80a34a57b5f3efb9bd30a5d431c4aedc8b0a
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-02-04 11:58:23 -0300

    ex 001

---
+++ exercicios/ex01.js
@@ -0,0 +1,14 @@
+/* Arquivo externo de JS */
+
+// Area das variaveis
+const Lutador = 'Viktor Ivanov';
+const Nacionalidade = 'Russo';
+const Idade = 29;
+const Peso = 93;
+const Altura = 1.88;
+
+document.write(`Lutador: ${Lutador} <br>`);
+document.write(`Nacionalidade: ${Nacionalidade} <br>`);
+document.write(`Idade: ${Idade} anos <br>`);
+document.write(`Peso: ${Peso} kg <br>`);
+document.write(`Altura: ${Altura} m <br>`);
